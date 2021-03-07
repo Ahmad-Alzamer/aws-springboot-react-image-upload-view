407e3230a5bf8e22e5f175afeb25f1fe090dfda6
ahmed.alzamer@gmail.com <ahmed.alzamer@gmail.com>
2021-03-06 20:23:42 -0500

    initialized a new Spring Boot project

---
+++ src/test/java/com/alzamer/ahmad/awsSpringbootReactImage/AwsSpringbootReactImageUploadViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alzamer.ahmad.awsSpringbootReactImage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootReactImageUploadViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
